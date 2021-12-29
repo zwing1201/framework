8446210c578e247bcb1d4712c221279826bb8e48
Zhang Yi <85141092@qq.com>
2021-12-29 11:38:36 +0800

    首次提交。

---
+++ framework.go
@@ -0,0 +1,7 @@
+package framework
+
+import "fmt"
+
+func Start()  {
+	fmt.Println("framework.Start1111")
+}
---
+++ log/log.go
@@ -0,0 +1,7 @@
+package log2
+
+import "fmt"
+
+func Log()  {
+	fmt.Println("Log..33333")
+}
